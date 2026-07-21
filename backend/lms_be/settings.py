9f9db570875a6f5f7e842b5ae8b9699eea42ec9b
tahir7892 <tahirawan7892@gmail.com>
2026-07-21 14:12:02 +0500

    ft: issue resolved for CORS

--- backend/lms_be/settings.py
+++ backend/lms_be/settings.py
@@ -119,7 +119,10 @@ USE_TZ = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
